a1a764a8e8adb3b128cca64ffb30f36cca727faf
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-06-11 23:35:30 +0100

    ruby models complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,8 +1,26 @@
+require_relative "entry.rb"
+
 class AddressBook
   attr_accessor :entries
 
-  def initialize(entries=[])
-    @entries = entries
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+    index = 0
+    @entries.each do |entry| 
+      if name < entry.name
+        break
+      end
+      index+=1
+    end
+
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(entry)
+    @entries.delete_at(entry)
   end
 
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -20,8 +20,34 @@ RSpec.describe AddressBook do
     end
   end
 
+  context "add_entry" do
+    it "should add an entry to an array" do
+      book = AddressBook.new
+      book.add_entry("Alex", "012345", "alex@yahoo.co.uk")
 
+      expect(book.entries.size).to eq 1
+    end
 
+    it "adds the correct information to entries" do
+      book = AddressBook.new
+      book.add_entry("Alex", "012345", "alex@yahoo.co.uk")
+      new_entry = book.entries[0]
 
+      expect(new_entry.name).to eq("Alex")
+      expect(new_entry.phone_number).to eq("012345")
+      expect(new_entry.email).to eq("alex@yahoo.co.uk")
+    end
+  end
+
+  context "remove_entry" do
+    it "should remove an entry from array" do 
+      book = AddressBook.new
+      book.add_entry("Alex", "012345", "alex@yahoo.co.uk")
+      entry = 0
+      book.remove_entry(entry)
+
+      expect(book.entries.size).to eq(0)
+    end
+  end
 
 end
