8b17c379100d46c42634710ec5e84d43c18eb4a9
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-06-20 13:05:03 +0100

    Completed last checkpoint

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -98,7 +98,45 @@ class MenuController
   end
 
   def search_entries
+    print "Enter a name: "
+    name = gets.chomp
 
+    match = @address_book.binary_search(name)
+    system "clear"
+
+    if match 
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No record was found for #{name}"
+    end
+  end
+
+  def search_submenu(entry)
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+    when d
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when e
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when m 
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
   end
 
   def read_csv
@@ -166,20 +204,6 @@ class MenuController
     system "clear"
     puts "Updated entry."
     puts entry
-  end
-
-  def search(name)
-    print "Enter name: "
-    name = gets.chomp
-
-    entry = @address_book.binary_search(name)
-    system "clear"
-
-    if entry
-      puts entry.to_s
-      search_submenu(match)
-    else
-      puts "Your serach di not return a match"
   end
 
 end
